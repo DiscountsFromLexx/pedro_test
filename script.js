25722c86dcd16e5a9d1572c3cb4975d9a50ae913
DISCOUNTS-FROM-AL-LEXX <dyntchev@gmail.com>
2026-02-17 15:00:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -276,7 +276,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <b>Статус відправлення (Cainiao)</b>
                     <iframe src="${trackUrl}" style="
                         width: 100%;
-                        height: 100%; // ← було 800px
+                        height: auto; // ← було 800px
                         border: none;
                         border-radius: 12px;
                         box-shadow: 0 4px 15px rgba(0,0,0,0.4);
